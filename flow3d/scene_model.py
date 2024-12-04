a6a2762f4a5447cd90842fbf674a28ed628cfcb5
Kai Katsumata <imperialroyalguard38@gmail.com>
2024-12-05 08:20:43 +0900

    Add debug prints for bg_color shape in SceneModel

--- flow3d/scene_model.py
+++ flow3d/scene_model.py
@@ -211,7 +211,7 @@ class SceneModel(nn.Module):
         if isinstance(bg_color, float):
             bg_color = torch.full((C, D), bg_color, device=device)
         assert isinstance(bg_color, torch.Tensor)
-
+        print(bg_color.shape)
         mode = "RGB"
         ds_expected = {"img": D}
 
@@ -222,6 +222,7 @@ class SceneModel(nn.Module):
             else:
                 mask_values = torch.ones((self.num_fg_gaussians, 1), device=device)
             colors_override = torch.cat([colors_override, mask_values], dim=-1)
+            print(bg_color.shape)
             bg_color = torch.cat([bg_color, torch.zeros(C, 1, device=device)], dim=-1)
             ds_expected["mask"] = 1
 
